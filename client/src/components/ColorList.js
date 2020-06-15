df9342b322479427a4cd1b8068b86f2e95eb2d3d
Elijah Atkins <elijahorama@gmail.com>
2020-06-14 19:50:34 -0600

    mvp done

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,45 +1,74 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  const { push } = useHistory();
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const editColor = color => {
+  const editColor = (color) => {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = (e) => {
     e.preventDefault();
+    console.log(colorToEdit);
+
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then((res) => {
+        updateColors([
+          ...colors.filter((x) => x.id !== colorToEdit.id),
+          res.data,
+        ]);
+        setEditing(false);
+      })
+      .catch((err) =>
+        console.error("ColorList.js: handleSubmit: ", err.message, err.response)
+      );
   };
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
+  const deleteColor = (id) => {
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${id}`)
+      .then((res) => {
+        updateColors(colors.filter((x) => x.id !== id));
+        push(`/BubblePage`);
+      })
+      .catch((err) =>
+        console.error(
+          "ColorList.js: handleDelete: err: ",
+          err.message,
+          err.response
+        )
+      );
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {colors.map((color) => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteColor(color.id);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -56,7 +85,7 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             color name:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
               }
               value={colorToEdit.color}
@@ -65,10 +94,10 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             hex code:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value }
+                  code: { hex: e.target.value },
                 })
               }
               value={colorToEdit.code.hex}
